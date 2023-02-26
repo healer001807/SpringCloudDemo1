052afa6e0d5872eade98b63c92cee30b358716d1
kw <kw@csii.com>
2023-02-26 15:35:28 +0800

    springCloud 服务

---
+++ Eureka-Zipkin/src/main/java/com/example/eurekazipkin/EurekaZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekazipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaZipkinApplication.class, args);
+    }
+
+}
